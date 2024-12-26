cc313dd8b355a46cc1f23a43080083f8855b3415
ben.azevedo <ben.azevedo@metergroup.com>
2024-12-26 09:42:24 -0800

    Updated script for starting, now npm run start and npx http-server E: --port 8080 run concurrently with concurrently

--- src/components/card/Card.tsx
+++ src/components/card/Card.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './card.css';
 import { IMovie } from '../../types/models';
-import { isElectron, resolvePathForElectron } from '../../utils/helpers';
 
 interface ICard {
   movie: IMovie;
@@ -11,9 +10,6 @@ interface ICard {
 function Card({ movie, onPlayMovie }: ICard) {
   const moviePath = `http://localhost:8080/${movie.title} (${movie.year}).mp4`;
 
-  // Use the helper to resolve the image path for Electron
-  const previewImgPath = resolvePathForElectron(movie.previewImg);
-
   return (
     <div className="col-lg-2 col-md-4 col-sm-6">
       <div
@@ -21,7 +17,7 @@ function Card({ movie, onPlayMovie }: ICard) {
         onClick={() => onPlayMovie(moviePath)}
         style={{ cursor: 'pointer' }}
       >
-        <img className="img-fluid" src={previewImgPath} alt={movie.title} />
+        <img className="img-fluid" src={movie.previewImg} alt={movie.title} />
         <div className="content">
           <h4>{movie.title}</h4>
           <div className="card-icons">
